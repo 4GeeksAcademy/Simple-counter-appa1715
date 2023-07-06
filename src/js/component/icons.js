351bff594528f5a2a394b300eb141abbd40328a8
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-07-06 06:53:52 +0000

    first js

---
+++ src/js/component/icons.js
@@ -0,0 +1,7 @@
+
+import fontawesome from '@fortawesome/fontawesome';
+import faClock from '@fortawesome/fontawesome-free-solid/faClock';
+fontawesome.config = {
+  autoReplaceSvg: 'nest'
+};
+fontawesome.library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
 
+
+function SimpleCounter(props) {
+    return <>
+        <div className="SecondCounter">
+            <div className="Clock"><i class="far fa-clock"></i></div>
+            <div className="Days">{props.Days}</div>
+            <div className="Hours">{props.Hours}</div>
+            <div className="Minutes">{props.Minutes}</div>
+            <div className="Seconds">{props.Seconds}</div>
+            <div className="Miliseconds">{props.Miliseconds}</div>
+        </div>
+    </>
+};
+
+let counter= 0;
+setInterval (function () {
+    ReactDOM.render
+    (<SimpleCounter/>, 
+    document.querySelector("#app"));
+},1000);
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
