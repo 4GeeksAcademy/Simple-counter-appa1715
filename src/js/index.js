0fe72d34e0a1dca2a30efea24004aa1e1fcfaaae
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-07-06 20:55:39 +0000

    done

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -14,19 +14,33 @@ function SimpleCounter(props) {
     return <>
         <div className="SecondCounter">
             <div className="Clock"><i class="far fa-clock"></i></div>
-            <div className="Days">{props.Days}</div>
-            <div className="Hours">{props.Hours}</div>
-            <div className="Minutes">{props.Minutes}</div>
-            <div className="Seconds">{props.Seconds}</div>
-            <div className="Miliseconds">{props.Miliseconds}</div>
+            <div className="Days">{props.Days % 10}</div>
+            <div className="Hours">{props.Hours % 10}</div>
+            <div className="Minutes">{props.Minutes % 10}</div>
+            <div className="Seconds">{props.Seconds % 10}</div>
         </div>
     </>
 };
+SimpleCounter.propTypes = {
+    Days: PropTypes.number,
+    Hours: PropTypes.number,
+    Minutes: PropTypes.number,
+    Seconds: PropTypes.number,
+
+}
 
 let counter= 0;
 setInterval (function () {
+    const Days = Math.floor(counter/1000);
+    const Hours = Math.floor(counter/100);
+    const Minutes = Math.floor(counter/10);
+    const Seconds = Math.floor(counter/1);
+    console.log(Days, Hours, Minutes, Seconds);
+
+
+    counter++;
     ReactDOM.render
-    (<SimpleCounter/>, 
+    (<SimpleCounter Seconds= {Seconds} Minutes= {Minutes} Hours= {Hours} Days= {Days}/>, 
     document.querySelector("#app"));
 },1000);
 
